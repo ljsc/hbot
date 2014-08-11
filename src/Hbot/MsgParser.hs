f84bd0079ece57ea2fd20bf8c93380b18af40bbb
Lou Scoras <louis.j.scoras@gmail.com>
2014-08-10 22:01:51 -0400

    QuickChecking parsing.
    Not sure this is worth it here. We're having to do a lot of work to
    create valid test input for something that is fairly trivial.
    Perhaps this would be better to just check with HUnit.

--- src/Hbot/MsgParser.hs
+++ src/Hbot/MsgParser.hs
@@ -21,6 +21,7 @@ module Hbot.MsgParser
     , BotCommand()
     , pluginName
     , messageText
+    , prefixParser
     ) where
 
 --------------------------------------------------------------------------------
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,12 +1,42 @@
-import Test.Tasty
-import Test.Tasty.QuickCheck as QC
+{-# LANGUAGE RecordWildCards #-}
+import Hbot.MsgParser
+
+import           Control.Applicative
+import           Data.Char
+import qualified Data.Text.Lazy as L
+import           Text.Parsec
+
+import           Test.Tasty
+import           Test.Tasty.QuickCheck as QC
 
 main = defaultMain tests
 
-tests = testGroup "Tests" [examples]
+tests = testGroup "Tests" [parsing]
 
-examples = testGroup "Examples"
-    [ QC.testProperty "trivial" $
-         \x -> (x :: Int) == x
+parsing = testGroup "Parsing"
+    [ QC.testProperty "Gets name and message text" $
+        \msg -> case parse (prefixParser $ prefix msg) "" (asText msg) of
+                    Right parsed -> commandName msg == pluginName parsed &&
+                                    remainder msg   == messageText parsed
+                    Left _ -> True
     ]
 
+data ServerMsg = ServerMsg
+    { spaces1     :: L.Text
+    , prefix      :: L.Text
+    , spaces2     :: L.Text
+    , commandName :: L.Text
+    , spaces3     :: L.Text
+    , remainder   :: L.Text
+    } deriving (Show)
+
+instance Arbitrary ServerMsg where
+    arbitrary = ServerMsg <$> spaces <*> anums
+                          <*> spaces <*> anums
+                          <*> spaces <*> anums --fmap L.pack arbitrary
+      where
+        spaces = L.pack <$> listOf1 (return ' ')
+        anums  = fmap L.pack $ listOf1 . suchThat (choose ('\33', '\122')) $ isAlphaNum
+
+asText :: ServerMsg -> L.Text
+asText (ServerMsg {..}) = L.concat $ [spaces1, prefix, spaces2, commandName, spaces3, remainder]
