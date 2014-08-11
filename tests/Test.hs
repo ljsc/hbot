31055e7822459663beadafd8aeb0163c12dd150f
Lou Scoras <louis.j.scoras@gmail.com>
2014-08-11 16:08:17 -0400

    Hooking up HUnit as well

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,12 +1,15 @@
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
 
 main = defaultMain tests
 
 tests = testGroup "Tests" [examples]
 
 examples = testGroup "Examples"
-    [ QC.testProperty "trivial" $
-         \x -> (x :: Int) == x
+    [ QC.testProperty "trivial property" $
+        \x -> (x :: Int) == x
+    , testCase "trivial unit test" $
+		1 + 1 @=? 2
     ]
 
